434a895b55814a2afabbb5da06bd6dc7d106b08c
atharv-av <atharvv333@gmail.com>
2024-02-28 14:41:33 +0530

    db setup and cloud connection

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const connectDatabase = require("./db/database");
 
-dotenv.config({path:"./.env"})
+// Config
+dotenv.config({ path: "./.env" });
+
+// Database Connection
+connectDatabase()
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
