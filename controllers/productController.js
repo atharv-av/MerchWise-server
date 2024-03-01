c09dc59f9ee1dfcf9686b267990fd9bbd5434716
atharv-av <atharvv333@gmail.com>
2024-03-01 13:43:17 +0530

    Created product CRUD APIs

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,87 @@
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route is working fine" });
+const Product = require("../models/productModel");
+
+// Create Product -- Admin
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+// List Products
+exports.getAllProducts = async (req, res) => {
+  const listProducts = await Product.find();
+  res.status(200).json({
+    success: true,
+    listProducts,
+  });
+};
+
+// Update Product --  Admin
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Delete Product -- Admin
+exports.deleteProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    await Product.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+
+// Get Product By Id
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "No product found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
 };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/productController");
+const {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getSingleProduct,
+} = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
+router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getSingleProduct)
 
 module.exports = router;
